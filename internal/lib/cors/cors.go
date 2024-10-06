c1fa4cae6d4a046c061505a896c4fc88e415b036
d0nil <kdi190104@gmail.com>
2024-10-06 20:01:08 +0300

    change cors origins

--- internal/lib/cors/cors.go
+++ internal/lib/cors/cors.go
@@ -4,7 +4,7 @@ import "github.com/rs/cors"
 
 func CorsSettings() *cors.Cors {
 	c := cors.New(cors.Options{
-		AllowedOrigins:     []string{"*"},
+		AllowedOrigins:     []string{"http://localhost:3000"},
 		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:     []string{"Authorization", "Content-Type"},
 		ExposedHeaders:     []string{"Authorization"},
